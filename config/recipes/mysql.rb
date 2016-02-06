cb33f2c9594efeb2c88875197f2320cc8c367bdd
Aakanksha <aakanksha@headerlabs.com>
2016-02-06 14:53:09 +0530

    "second commit"

--- config/recipes/mysql.rb
+++ config/recipes/mysql.rb
@@ -2,6 +2,6 @@ namespace :mysql do
 
   desc "Create a database for this application."
   task :create_database => :environment do
-    queue! %[echo "cd  #{current_path}; bundle exec rake db:create RAILS_ENV=staging"]
+    queue! %[echo "cd  #{current_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"]
   end
 end
