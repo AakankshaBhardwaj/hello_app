fd3cb5f15b083c2d78c12df2c3f3006afe40d9a5
Aakanksha <aakanksha@headerlabs.com>
2016-02-06 13:16:40 +0530

    "fixing path for nginx

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -4,7 +4,7 @@ namespace :nginx do
   task :install => :environment do
     queue! %[sudo -A apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7]
     queue! %[sudo -A apt-get install -y apt-transport-https ca-certificates]
-    queue! %[sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger wily main > /etc/apt/sources.list.d/passenger.list']
+    queue! %[sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list']
     # queue! %[sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger precise main > /etc/apt/sources.list.d/passenger.list']
     queue! %[sudo apt-get update]
     queue! %[sudo apt-get install -y nginx-extras passenger]
