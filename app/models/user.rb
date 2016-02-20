29bb6204fb040cdb3fd45e2c4bfe3c1b8d06df24
Aakanksha <aakanksha@headerlabs.com>
2016-02-20 10:38:12 +0530

    "adding initializers and locals"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  require 'benchmark'
+  def say_hello
+    n = 50000
+    Benchmark.bmbm do |x|
+      x.report('name'){
+      User.last
+      }
+    end
+  end
 end
